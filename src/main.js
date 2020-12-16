bccfd294d11207143c3efeaaac3c82ada8e1ad0e
syx457 <shiyuxing1222@163.com>
2020-12-16 18:19:09 +0800

    '详情页已经完成'

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import router from "@/router";
 import store from './store'
 import toast from '@/components/common/toast'
 
+import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
+
 Vue.config.productionTip = false
 
 //添加事件总线
 Vue.prototype.$bus = new Vue()
+
 //安装toast插件
 Vue.use(toast)
+
+//安装lazyload
+Vue.use(VueLazyload, {
+  loading: require('./assets/img/common/placeholder.png')
+})
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
